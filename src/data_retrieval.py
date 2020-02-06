a31aeb35b80b9b876bc77df40453f25065f9af30
Alp Aribal <alparbal@gmail.com>
2020-02-06 01:17:05 +0100

    add logging to file, make file downloads async

--- src/data_retrieval.py
+++ src/data_retrieval.py
@@ -1,22 +1,21 @@
-import requests
+import asyncio
 import json
+import logging
+import shutil
+import sys
+from datetime import datetime, timedelta
 from pathlib import Path
 from time import sleep
-import shutil
-import logging
-import sys
-from datetime import timedelta, datetime
 from typing import List, Tuple
-from tqdm import tqdm
 
+import httpx
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-ch = logging.StreamHandler(sys.stdout)
-ch.setFormatter(format)
-logger.addHandler(ch)
+logging_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 
 ALLOWED_TYPES = (
@@ -29,47 +28,71 @@ ALLOWED_TYPES = (
 )
 
 
-def download_bulk_in_batches(
+async def download_bulk_in_batches(
     start_date: str,
     end_date: str,
     dl_path: str,
     award_types: List[str] = ALLOWED_TYPES,
     batch_size: int = 4,
+    sleep_time: int = 1,
 ) -> None:
     """Download awards data in batches.
 
+    Batches are downloaded asynchronously.
+
     Parameters:
         start_date: str
-            Beginning of date range for which bulk awards data will be downloaded. Its format must be 'yyyy-mm-dd'.
+            Beginning of date range for which bulk awards data will be downloaded.
+            Its format must be 'yyyy-mm-dd'.
         end_date: str
-            End of date range for which bulk awards data will be downloaded. Its format must be 'yyyy-mm-dd'.
+            End of date range for which bulk awards data will be downloaded.
+            Its format must be 'yyyy-mm-dd'.
         dl_path: Pathlike
             Path to the folder in which awards data batches will be downloaded.
         award_types: str (opt)
             The specific type(s) of the award to be downloaded.
         batch_size: int (opt)
             Size of each batch in number of weeks.
+        sleep_time: int (opt)
+            Number of seconds to wait before rechecking file download links.
     """
+    # Enable logging to a log file
+    log_file = "download.log"
+    ch = logging.FileHandler(log_file, "w")
+    ch.setFormatter(logging_format)
+    logger.addHandler(ch)
+    print(f"Starting bulk download. Progress will be logged in {log_file}.")
 
     # Assert that the inputs are of correct format
-    assert isinstance(start_date, str), "`start_date` must be of type `str`."
-    assert isinstance(end_date, str), "`end_date` must be of type `str`."
-    assert isinstance(dl_path, str), "`dl_path` must be of type `str`."
+    if not isinstance(start_date, str):
+        raise ValueError("`start_date` must be of type `str`.")
+
+    if not isinstance(end_date, str):
+        raise ValueError("`end_date` must be of type `str`.")
+
+    if not isinstance(dl_path, str):
+        raise ValueError("`dl_path` must be of type `str`.")
 
     dl_path = Path(dl_path)
-    assert dl_path.is_dir(), "`dl_path` must be a path to a valid folder."
+    if not dl_path.is_dir():
+        raise ValueError("`dl_path` must be a path to a valid folder.")
 
-    assert isinstance(award_types, List) or isinstance(
-        award_types, Tuple
-    ), "`award_types` must be of type `List[str]`."
-    assert isinstance(batch_size, int), "`batch_size` must be of type `int`."
+    if not isinstance(award_types, List) and not isinstance(award_types, Tuple):
+        raise ValueError("`award_types` must be of type `List[str]`.")
 
-    assert len(award_types) > 0, "`award_types` cannot be empty."
+    if len(award_types) == 0:
+        raise ValueError("`award_types` cannot be empty.")
+
+    if not isinstance(batch_size, int):
+        raise ValueError("`batch_size` must be of type `int`.")
+
     # Check whether given award types are allowed by USAspending API
     for a_type in award_types:
-        assert (
-            a_type in ALLOWED_TYPES
-        ), f"Disallowed award type: {a_type}. Allowed award types are {ALLOWED_TYPES}."
+        if a_type not in ALLOWED_TYPES:
+            raise ValueError(
+                f"Disallowed award type: {repr(a_type)}. Allowed award types are"
+                f" {str(ALLOWED_TYPES)}."
+            )
 
     start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
     end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
@@ -88,6 +111,7 @@ def download_bulk_in_batches(
     batch_date_list.append((batch_start, batch_end))
 
     logger.debug(f"Number of batches: {len(batch_date_list)}")
+
     # Initialize the progress bar
     pbar = tqdm(total=len(batch_date_list))
 
@@ -106,13 +130,16 @@ def download_bulk_in_batches(
         }
 
         headers = {"Content-Type": "application/json"}
-        response = requests.post(
+        response = httpx.post(
             "https://api.usaspending.gov/api/v2/bulk_download/awards/",
             headers=headers,
             data=json.dumps(req),
         )
 
-        assert response, f"USAspending API returned error: `{response.text}`.`"
+        if not response:
+            raise ConnectionError(
+                f"USAspending API returned error: `{response.text}`.`"
+            )
 
         response = response.json()
         file_url = response["file_url"]
@@ -121,21 +148,52 @@ def download_bulk_in_batches(
 
         wait_list.append((file_name, file_url))
 
-    while wait_list:
-        for file_name, file_url in wait_list:
-            # Check whether the file is ready to download
-            file_ready = requests.head(file_url)
-            if file_ready:
-                logger.debug(f"`{file_name}` is ready, starting download.")
-                # Stream the file and write to file on disk
-                with requests.get(file_url, stream=True) as r:
-                    with open(str(dl_path.joinpath(file_name)), "wb") as f:
-                        shutil.copyfileobj(r.raw, f)
-                wait_list.remove((file_name, file_url))
-                logger.debug("Download finished, moving to the next.")
-                pbar.update(1)
-        if wait_list:
-            # If there are jobs left, wait for a while before re-checking the urls
-            sleep(1)
+    await asyncio.gather(
+        *[
+            download_file(file_name, file_url, dl_path, sleep_time, logger, pbar)
+            for (file_name, file_url) in wait_list
+        ]
+    )
 
     logger.debug("All downloads are completed.")
+    logger.removeHandler(ch)
+
+
+async def download_file(
+    file_name, file_url, dl_path, sleep_time=1, logger=None, pbar=None
+):
+    """Download single awards file asynchronously.
+
+    Parameters:
+        file_name: str
+            Name of the file to download.
+        file_url: str
+            Url of the file.
+        dl_path: Pathlike
+            Path to the folder in which awards data batches will be downloaded.
+        sleep_time: int (opt)
+            Number of seconds to wait before rechecking file download links.
+        logger: 
+            Logger to use when logging download events.
+        pbar: tqdm.tqdm
+            tqdm object to update when the download is finished.
+    """
+    file_ready = False
+    while not file_ready:
+        await asyncio.sleep(sleep_time)
+        file_ready = httpx.head(file_url)
+
+    if logger:
+        logger.debug(f"`{file_name}` is ready, starting download.")
+
+    async with httpx.AsyncClient() as client:
+        async with client.stream("GET", file_url) as r:
+            with open(str(dl_path.joinpath(file_name)), "wb") as f:
+                async for a in r.aiter_raw():
+                    f.write(a)
+
+    if logger:
+        logger.debug(f"Download of `{file_name}` is finished.")
+
+    if pbar:
+        pbar.update(1)
